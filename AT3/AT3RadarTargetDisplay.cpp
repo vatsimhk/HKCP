baabcd5d57fbb4811bbb9c9bf6c9d460129eab1e
Justin Wai <justin.wai@gmail.com>
2024-07-01 21:41:47 +0800

    Implement CJS label on top of aircraft icon

--- AT3/AT3RadarTargetDisplay.cpp
+++ AT3/AT3RadarTargetDisplay.cpp
@@ -22,8 +22,31 @@ void AT3RadarTargetDisplay::OnRefresh(HDC hDC, int Phase, HKCPDisplay* Display)
 	CDC dc;
 	dc.Attach(hDC);
 
+	// Save context for later
+	int sDC = dc.SaveDC();
+
 	// Create graphics object
 	Graphics g(hDC);
+
+	// Create font
+	CFont EuroScopeFont;
+	EuroScopeFont.CreateFont(
+		16,                       // nHeight
+		0,                        // nWidth
+		0,                        // nEscapement
+		0,                        // nOrientation
+		FW_NORMAL,                // nWeight
+		FALSE,                    // bItalic
+		FALSE,                    // bUnderline
+		0,                        // cStrikeOut
+		ANSI_CHARSET,             // nCharSet
+		OUT_DEFAULT_PRECIS,       // nOutPrecision
+		CLIP_DEFAULT_PRECIS,      // nClipPrecision
+		DEFAULT_QUALITY,          // nQuality
+		DEFAULT_PITCH | FF_SWISS, // nPitchAndFamily
+		_T("EuroScope")  // lpszFacename
+	);      
+	
 
 	// Select first aircraft
 	CRadarTarget acft;
@@ -51,15 +74,23 @@ void AT3RadarTargetDisplay::OnRefresh(HDC hDC, int Phase, HKCPDisplay* Display)
 
 		// Set brush color based on state
 		SolidBrush aircraftBrush(DEFAULT_UNCONCERNED);
+		dc.SetTextColor(DEFAULT_UNCONCERNED.ToCOLORREF());
 		if (fp.GetState() == FLIGHT_PLAN_STATE_ASSUMED) {
 			aircraftBrush.SetColor(DEFAULT_ASSUMED);
+			dc.SetTextColor(DEFAULT_ASSUMED.ToCOLORREF());
+		}
+		else if (fp.GetState() == FLIGHT_PLAN_STATE_TRANSFER_FROM_ME_INITIATED) {
+			aircraftBrush.SetColor(DEFAULT_ASSUMED);
+			dc.SetTextColor(DEFAULT_REDUNDANT.ToCOLORREF());
 		}
 		else if (fp.GetState() == FLIGHT_PLAN_STATE_REDUNDANT || fp.GetState() == FLIGHT_PLAN_STATE_TRANSFER_TO_ME_INITIATED) {
 			aircraftBrush.SetColor(DEFAULT_REDUNDANT);
+			dc.SetTextColor(DEFAULT_REDUNDANT.ToCOLORREF());
 		}
 
 		// Get and set location
 		POINT acftLocation = Display->ConvertCoordFromPositionToPixel(acft.GetPosition().GetPosition());
+		PointF acftLabelLocation = PointF(acftLocation.x, acftLocation.y + 15);
 		g.TranslateTransform(acftLocation.x, acftLocation.y, MatrixOrderAppend);
 		g.RotateTransform(acft.GetPosition().GetReportedHeadingTrueNorth());
 
@@ -92,12 +123,31 @@ void AT3RadarTargetDisplay::OnRefresh(HDC hDC, int Phase, HKCPDisplay* Display)
 		// Draw the aircraft icon
 		g.FillPolygon(&aircraftBrush, aircraftIcon, 19);
 
+		// Cleanup
 		g.EndContainer(gContainer);
 		DeleteObject(&aircraftIcon);
+
+		if (fp.GetState() == FLIGHT_PLAN_STATE_ASSUMED) {
+			acft = GetPlugIn()->RadarTargetSelectNext(acft);
+			continue;
+		}
+
+		// Draw CJS
+		dc.SelectObject(EuroScopeFont);
+		dc.SetTextAlign(TA_CENTER);
+		if (fp.GetState() == FLIGHT_PLAN_STATE_TRANSFER_FROM_ME_INITIATED) {
+			dc.TextOutA(acftLocation.x, acftLocation.y - 30, fp.GetHandoffTargetControllerId());
+		}
+		else {
+			dc.TextOutA(acftLocation.x, acftLocation.y - 30, fp.GetTrackingControllerId());
+		}
 
 		// Increment to next aircraft
 		acft = GetPlugIn()->RadarTargetSelectNext(acft);
 	}
+
+	// Restore context
+	dc.RestoreDC(sDC);
 
 	//De-allocate graphics objects
 	dc.Detach();
