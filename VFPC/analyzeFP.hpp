17c7f4223c4716ce9630ae758e92b2cc620e1ec2
Justin Wai <justin.wai@gmail.com>
2024-10-05 21:29:17 -0400

    Clear existing VFPC implementation to prepare for rewrite

--- VFPC/analyzeFP.hpp
+++ VFPC/analyzeFP.hpp
@@ -7,14 +7,9 @@
 #include <fstream>
 #include <vector>
 #include <map>
-#include <boost/algorithm/string.hpp>
-#include <boost/format.hpp>
-#include "rapidjson/document.h"
-#include "rapidjson/stringbuffer.h"
+#include <nlohmann/json.hpp>
 
 using namespace std;
-using namespace boost;
-using namespace rapidjson;
 using namespace EuroScopePlugIn;
 
 class CVFPCPlugin :
@@ -24,105 +19,11 @@ public:
 	CVFPCPlugin();
 	virtual ~CVFPCPlugin();
 
-	virtual void getSids();
+	void sendMessage(string type, string message);
 
-	virtual map<string, string> validizeSid(CFlightPlan flightPlan);
-
-	virtual void OnFunctionCall(int FunctionId, const char * ItemString, POINT Pt, RECT Area);
-
-	//Define OnGetTagItem function
-	virtual void OnGetTagItem(CFlightPlan FlightPlan,
-		CRadarTarget RadarTarget,
-		int ItemCode,
-		int TagData,
-		char sItemString[16],
-		int* pColorCode,
-		COLORREF* pRGB,
-		double* pFontSize);
-
-	template <typename Out>
-	void split(const string& s, char delim, Out result) {
-		istringstream iss(s);
-		string item;
-		while (getline(iss, item, delim)) {
-			*result++ = item;
-		}
-	}
-
-	vector<string> split(const string& s, char delim) {
-		vector<string> elems;
-		split(s, delim, back_inserter(elems));
-		return elems;
-	}
-
-	string destArrayContains(const Value& a, string s) {
-		for (SizeType i = 0; i < a.Size(); i++) {
-			string test = a[i].GetString();
-			SizeType x = static_cast<rapidjson::SizeType>(s.rfind(test, 0));
-			if (s.rfind(a[i].GetString(), 0) != -1)
-				return a[i].GetString();
-		}
-		return "";
-	}
-
-	bool arrayContains(const Value& a, string s) {
-		for (SizeType i = 0; i < a.Size(); i++) {
-			if (a[i].GetString() == s)
-				return true;
-		}
-		return false;
-	}
-
-	bool arrayContains(const Value& a, char s) {
-		for (SizeType i = 0; i < a.Size(); i++) {
-			if (a[i].GetString()[0] == s)
-				return true;
-		}
-		return false;
-	}
-
-	string arrayToString(const Value& a, char delimiter) {
-		string s;
-		for (SizeType i = 0; i < a.Size(); i++) {
-			s += a[i].GetString()[0];
-			if (i != a.Size() - 1)
-				s += delimiter;
-		}
-		return s;
-	}
-	bool routeContains(string s, const Value& a) {
-		for (SizeType i = 0; i < a.Size(); i++) {
-			bool dd = contains(s, a[i].GetString());
-			if (contains(s, a[i].GetString()))
-				return true;
-		}
-		return false;
-	}
-
-	virtual void OnFlightPlanDisconnect(CFlightPlan FlightPlan);
-	
-	virtual bool OnCompileCommand(const char * sCommandLine);
-
-	virtual void debugMessage(string type, string message);
-
-	virtual void sendMessage(string type, string message);
-
-	virtual void sendMessage(string message);
-
-	virtual void checkFPDetail();
-
-	virtual void checkFLAS();
-
-	virtual void flUp(CFlightPlan FlightPlan);
-
-	virtual void flDown(CFlightPlan FlightPlan);
-
-	virtual pair<string, int> getFails(map<string, string> messageBuffer);
-
-	virtual void OnTimer(int Count);
+	void sendMessage(string message);
 
 protected:
-	Document config;
-	map<string, rapidjson::SizeType> airports;
+
 };
 
